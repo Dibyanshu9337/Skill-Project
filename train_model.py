87a87e979a62c8ef09b92111e76ca0c9b5069a44
Dibyanshu9337 <165541871+Dibyanshu9337@users.noreply.github.com>
2026-02-26 19:16:06 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -7,7 +7,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
-df = pd.read_csv("data/mental_health_large.csv")
+df = pd.read_csv("mental_health_large.csv")
 
 X_train, X_test, y_train, y_test = train_test_split(
     df["text"], df["label"], test_size=0.2, random_state=42
